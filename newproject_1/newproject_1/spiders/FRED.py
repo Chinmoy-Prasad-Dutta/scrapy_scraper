09f6abfc3bcf10ee28f486d83b450c89a07e066e
Chinmoy Dutta <92702324+Chinmoy-Prasad-Dutta@users.noreply.github.com>
2022-04-01 14:07:17 +0530

    updated the code.
    Changed the code a little to accommodate some pages with different html codes and different CSS formatting.
    Scraped a total of 20078 web pages and added the final output file.

--- newproject_1/newproject_1/spiders/FRED.py
+++ newproject_1/newproject_1/spiders/FRED.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 
@@ -8,22 +9,44 @@ class FredSpider(scrapy.Spider):
     start_urls = ['https://fred.stlouisfed.org/categories']
 
     def parse(self, response):
-        links = response.css("span.fred-categories-child a::attr(href)").getall()[0:3]
+        links = response.css("span.fred-categories-child a::attr(href)").getall()
         for link in links:
-            yield response.follow(link, callback=self.parse_blog_post)
+            yield response.follow(link, callback=self.parse_blog_post_1)
         
     
+    """unused code for reference as some href have diff structure followup links"""
+    # def parse_blog_post(self, response):
+    #     for div_selector in response.css('subcats.col-xs-12'):
+    #         links4 = div_selector.css('div.col-xs-12.col-sm-6 a::attr(href)').getall()
+    #         # linkextra = div_selector.css('div.col-xs-12.col-sm-10 a::attr(href)').getall()
+        
     
+    #         """for loop to go through the list4 and follow the links"""
+    #         for link in links4:
+    #             yield response.follow(link, callback=self.parse_blog_post_1)
+
+
     
-    def parse_blog_post(self, response):
-        for div_selector in response.css('div.col-xs-12.panel-menu'):
-            links2 = div_selector.css('ul.list-bullets a::attr(href)').getall()
+    def parse_blog_post_1(self, response):
+        for div_selector in response.css('div.col-xs-12.col-sm-6'):
+            links2 = [re.sub(' ', '', ele) for ele in div_selector.css('ul.list-bullets a::attr(href)').getall()] 
+            """used list comprehension to remove spaces 
+            from href as they were not providing correct followup links"""
             for link in links2:
                 yield response.follow(link, callback=self.parse_blog_post_2)
-            # {
-            #     'title': div_selector.css('ul.list-bullets a::attr(href)').getall(),
-            #     'text': div_selector.css('ul.list-bullets a::text').getall()
-            # }
+        
+        """this directly calls back to the last function as there are no more followup links"""
+        for div_selector in response.css('div.col-xs-12.col-sm-10'):
+            links2 = response.css('div.col-xs-12.col-sm-10 a::attr(href)').getall()
+            
+            for link in links2:
+                yield response.follow(link, callback=self.parse_blog_post_3)
+        
+    """this code can be directly used to get the series id for certian webpages"""
+    # def parse_blog_post_x(self, response):
+    #     linkextra = response.css('div.col-xs-12.col-sm-10 a::attr(href)').getall()
+    #     for link in linkextra:
+    #         yield response.follow(link, callback=self.parse_blog_post_3)
     
     def parse_blog_post_2(self, response):
         for table_selector in response.css('table.table-responsive'):
@@ -40,13 +63,4 @@ class FredSpider(scrapy.Spider):
         yield{
             'name' : response.css('h1.series-title span::text').getall()[1],
             'series_id' : response.css('h1.series-title a::attr(data-series-id)').getall(),
-            
-            
-        }
-        
-    
-    
-    
-    
-    
-    
+            }
